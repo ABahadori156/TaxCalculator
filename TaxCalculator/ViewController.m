2fcb9eff0430d47005ede48e0c2685e39c05eee1
Pasha Bahadori <Bahadori@pashas-macbook-pro.local>
2016-05-11 19:15:41 -0700

    Tax Calculator Joker

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -38,7 +38,7 @@
         double result = enteredValue * self.chiTax;
         self.resultLabel.text = [NSString stringWithFormat:@"$%.2f", result];
     }
-    
+    //Joker
 }
 
 - (void)viewDidLoad {
